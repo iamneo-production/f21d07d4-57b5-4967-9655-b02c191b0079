823ef5f85611208f1f16a8f61c740abe5a41546c
krishnavarma0100 <krishnavarma0100@gmail.com>
2023-06-18 00:28:40 +0000

    Added code for user database

---
+++ springapp/src/main/java/com/example/springapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.springapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.springapp.model.UserModel;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, String> {
+    
+}
