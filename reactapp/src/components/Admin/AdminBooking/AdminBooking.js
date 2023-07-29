a7e37edd4f1324c34255e325fe7a4987a3cb504e
poojitha0812 <131637925+poojitha0812@users.noreply.github.com>
2023-07-29 20:19:51 +0530

    Update AdminBooking.js

--- reactapp/src/components/Admin/AdminBooking/AdminBooking.js
+++ reactapp/src/components/Admin/AdminBooking/AdminBooking.js
@@ -1,1 +1,109 @@
+/*import React from 'react';
+import { Formik, Form, ErrorMessage } from 'formik';
+import { TextField } from './TextField';
+import * as Yup from 'yup';
+import axios from 'axios';
+import styles from './AdminAddCentreForm.module.css';
+import { useNavigate } from 'react-router-dom';
+import * as myaxios from '../../../api/myaxios';
 
+function AdminAddCentreForm() {
+
+  const navigate = useNavigate();
+
+  //Yup library to define the form validation rules for each input field in the form.
+  // These rules specify the required fields, max length, valid email format, valid phone number format, etc.
+
+  const validate = Yup.object({
+    name: Yup.string().max(25, 'Should be less than 15 characters').required('Name is required'),
+    mobileNumber: Yup.string()
+      .matches("^[0-9]{10}$", 'Phone number is not valid')
+      .required('Required'),
+    address: Yup.string().max(50, 'Should be less than 50 characters').required('Required'),
+    city: Yup.string().required('City is a required field').max(25, 'Should be less than 25 characters'),
+    pincode: Yup.string().max(6, 'Invalid Pincode').min(6, 'Invalid Pincode').required('Pincode is a required field'),
+    imgUrl: Yup.string().url('Invalid URL'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    description: Yup.string().max(100, 'Should not exceed 100 characters'),
+  });
+
+  //make an HTTP POST request to the server, presumably to add a new service center with the form data (val).
+  async function handleOnSubmit(val) {
+    try {
+      const res = await myaxios.addCenter(val);
+      alert('Centre Added Successfully');
+      navigate('/admin/home');
+    } catch (error) {
+      console.log(error);
+      alert('Add Centre Failed');
+    }
+  }
+// component wraps the form and handles form state, validation, and form submission logic.
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        mobileNumber: '',
+        address: '',
+        city: '',
+        pincode: '',
+        imgUrl: '',
+        email: '',
+        description: '',
+      }}
+
+      //The validationSchema prop is set to validate, which enforces the defined validation rules on the form inputs.
+      validationSchema={validate}
+      onSubmit={(values, { resetForm }) => {
+        handleOnSubmit(values);
+        resetForm({ values: '' });
+      }}
+    >
+      {formik => (
+        <div className={styles.container}>
+          <h1 className={styles.title}>Add Centre</h1>
+          <Form className={styles.form}>
+            <TextField id="addName" label="Name" name="name" type="text" />
+            <TextField id="addNumber" label="Phone Number" name="mobileNumber" type="text" />
+            <TextField id="addAddress" label="Address" name="address" type="text" />
+            <TextField id="addCity" label="City" name="city" type="text" />
+            <TextField id="addPincode" label="Pincode" name="pincode" type="text" />
+            <TextField id="addImageUrl" label="Image URL" name="imgUrl" type="text" />
+            <TextField id="addEmail" label="Email" name="email" type="email" />
+            <TextField id="addCentreDescription" label="Description" name="description" type="textarea" />
+            <ErrorMessage name="description" component="div" className={styles.error} />
+            <button className={styles.button} id="addButton" type="submit">
+              Add Centre
+            </button>
+          </Form>
+        </div>
+      )}
+    </Formik>
+  );
+}
+
+export default AdminAddCentreForm;*/
+import React from "react";
+import './AdminBooking.module.css'
+import 'react-toastify/dist/ReactToastify.css';
+
+//The booking component is a functional component that receives props as its parameter. It appears to represent a row of booking data in a table.
+const booking = (props) => {
+
+  //The props object is used to access the booking data for each column in the table row:
+    return (  
+      <>
+        <tr>
+          <td>{props.data.appointmentId}</td>
+          <td>{props.data.centerName}</td>
+          <td>{props.data.userName}</td>
+          <td>{props.data.productName}</td>
+          <td>{props.data.bookingDate}</td><br></br>
+          <td>{props.data.bookingTime}</td> 
+        </tr>
+      </>
+                      
+  );
+};
+  
+export default booking;
