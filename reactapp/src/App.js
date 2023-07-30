7314f3c201bbf302561c5300d16b8fb86145385a
Jyotishmathi <jyotishmathi01@gmail.com>
2023-07-30 11:45:01 +0000

    commits made

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import {Routes, Route, BrowserRouter} from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import './App.css';
+import LandingPage from './pages/LandingPage/LandingPage';
+import AdminHomePage from './pages/Admin/Home/AdminHomePage';
+import AdminAddCentrePage from './pages/Admin/AddCenter/AdminAddCentrePage';
+import AdminLayout from './pages/Admin/Layout/AdminLayout';
+import Adminbooking from './pages/Admin/BookingsPage/Adminbooking';
+import UserLayOut from './pages/User/Layout/UserLayOut';
+import Centerprofilescreen from './pages/Admin/CenterProfile/Centerprofilescreen';
+import { ErrorPage } from './pages/ErrorPage/ErrorPage';
+import  UserHomePage  from './pages/User/Home/UserHomePage';
+import UserDashboard from './pages/User/Dashboard/UserDashboard';
+import Userbooking from './pages/User/MyBookings/Userbooking';
+import Usereditcenter from './pages/User/EditAppoinment/Usereditcenter';
+import AvailableSlots from './components/User/AvailableSlots/AvailableSlots';
+import PaymentCard from './components/PaymentCard/PaymentCard';
+import Userreview from './pages/User/Userreview/Userreview';
+import Login from "./components/Auth/Login/Login"
+import Signup from "./components/Auth/Signup/Signup"
 
-export default function App() {
+function App() {
   return (
-   <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" exact element={<Login />}></Route>
-        <Route path="/signup" exact element={<Signup />}></Route>
-        
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/admin" element={<AdminLayout/>}>
+          <Route path="/admin/home" element={<AdminHomePage/>}></Route>
+          <Route path="/admin/add-centre" element={<AdminAddCentrePage/>}></Route>
+          <Route path="/admin/edit-center" element={<Centerprofilescreen/>}></Route>
+          <Route path="/admin/bookings" element={<Adminbooking/>}></Route>
+        </Route>
+        <Route path="/user" element={<UserLayOut/>}>
+          <Route path='/user/home' element={<UserHomePage/>}></Route>
+          <Route path='/user/EditCenter' element={<Usereditcenter/>}></Route>
+          <Route path="/user/booking/slot" element={<AvailableSlots/>}></Route>
+          <Route path="/user/payment" element={<PaymentCard/>}></Route>
+          <Route path='/user/Mybooking' element={<Userbooking/>}></Route>
+          <Route path='/user/dashboard' element={<UserDashboard/>}></Route>
+          <Route path="/user/center/reviews/:centerId" element={<Userreview/>}></Route>
+        </Route>
+
+        <Route path="*" element={<ErrorPage/>}></Route>
       </Routes>
     </BrowserRouter>
-   </>
-  )
+  );
 }
+export default App;
---
+++ reactapp/src/components/Auth/Signup/Signup.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { TextField } from '../TextField';
+import { Select, MenuItem, FormControl, InputLabel } from "@mui/material";
+import * as Yup from "yup";
+import { Link } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import * as myaxios from "../../../api/myaxios";
+import rocketImg from '../../../assets/rocket.png'
+
+export default function Signup(){
+  const validate = Yup.object({
+    name: Yup.string().max(25, "Must be 15 characters or less").required("Required"),
+    email: Yup.string().email("Email is invalid").required("Required"),
+    mobileNumber: Yup.string()
+      .matches("^[0-9]{10}$", "Phone number is not valid")
+      .required("Required"),
+    password: Yup.string().min(6, "Password must be at least 6 characters").required("Required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Password must match")
+      .required("Required"),
+  });
+
+  const handleOnSubmit = async (values) => {
+    try {
+      const res = await myaxios.signup(values);
+      if (res.data === "Email id already exists" || res.data === "Mobile number already exists") {
+        toast.error(res.data);
+      } else {
+        toast.success("SIGNUP SUCCESSFULL", { position: "top-center", autoClose: 2000 });
+        setTimeout(() => {
+          window.location.replace("/login");
+        }, 2000);
+      }
+    } catch (err) {
+      toast.error("SIGNUP FAILED !");
+    }
+  };
+
+  return (
+    <div className='container mt-3'>
+    <div className='row'>
+      <div className='col-md-5'>
+    <Formik
+      initialValues={{
+        userType: "",
+        name: "",
+        email: "",
+        mobileNumber: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={validate}
+      onSubmit={(values, { resetForm }) => {
+        handleOnSubmit(values);
+        resetForm({ values: "" });
+      }}
+    >
+      {(formik) => (
+        <div>
+          <h1 className="my-4 font-weight-bold-display-4">Sign Up</h1>
+          <Form>
+            <FormControl>
+              <InputLabel id="user-type-label"
+                sx={{
+                  marginTop: "35px",
+                }}
+              >User-Type</InputLabel>
+              <Select
+                sx={{
+                  marginTop: 5,
+                  width: 525,
+                  height: 50,
+                }}
+                labelId="user-type-label"
+                id="admin/user"
+                name="userType"
+                value={formik.values.userType}
+                onChange={formik.handleChange}
+              >
+                <MenuItem value={"USER"}>USER</MenuItem>
+                <MenuItem value={"ADMIN"}>ADMIN</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField data-testid="username" label="Name" name="name" type="text" />
+            <TextField data-testid="email" label="Email" name="email" type="text" />
+            <TextField data-testid="mobileNumber" label="Mobile Number" name="mobileNumber" type="text" />
+            <TextField data-testid="password" label="Password" name="password" type="password" />
+            <TextField
+              id="confirmPassword"
+              label="Confirm Password"
+              name="confirmPassword"
+              type="password"
+            />
+            <button data-testid='submitButton' className="btn btn-dark mt-3" type="submit">
+              Register
+            </button>
+          </Form>
+          <br />
+          <p>
+            Already have an account? <Link data-testid='signinLink' to="/login">Login</Link>
+          </p>
+          <ToastContainer />
+        </div>
+      )}
+    </Formik>
+    </div>
+        <div className='col-md-7 my-auto'>
+          <img className="img-fluid w-100" src={rocketImg} alt='' />
+        </div>
+      </div>
+   </div>
+  );
+};
