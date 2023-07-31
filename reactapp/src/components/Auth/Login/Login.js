8af65d51ccbcdd459d50fb6b7674e0d1adaebb2e
Shinde Payal Vasant <101012572+payalshinde15@users.noreply.github.com>
2023-07-31 11:43:39 +0530

    Update Login.js

--- reactapp/src/components/Auth/Login/Login.js
+++ reactapp/src/components/Auth/Login/Login.js
@@ -1,9 +1,4 @@
-
-     
-  
-             
-                  
-   import React from "react";
+import React from "react";
 import { Formik, Form } from "formik";
 import { TextField } from "../TextField";
 import { Link } from "react-router-dom";
